53c95702251e8e210a3209b77e68591ec88a00fb
zeimedee <42755623+zeimedee@users.noreply.github.com>
2024-11-05 15:24:05 +0000

    setup services

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -3,12 +3,14 @@ package main
 import (
 	"os"
 
+	"github.com/zeimedee/sre_bootcamp/internal/database"
 	"github.com/zeimedee/sre_bootcamp/internal/routes"
 )
 
 func main() {
-	port := os.Getenv("PORT")
 
+	port := os.Getenv("PORT")
+	database.ConnectDb()
 	router := routes.SetUpRouter()
 
 	router.Run(port)
--- internal/services/services.go
+++ internal/services/services.go
@@ -1,23 +1,44 @@
 package services
 
-import "github.com/zeimedee/sre_bootcamp/internal/models"
+import (
+	"fmt"
+	"log"
+
+	"github.com/zeimedee/sre_bootcamp/internal/database"
+	"github.com/zeimedee/sre_bootcamp/internal/models"
+)
 
 func CreateNewStudent(student models.Student) (models.Student, error) {
-	return models.Student{}, nil
+	database.DB.Db.Create(student)
+	return student, nil
 }
 
 func GetStudent(id string) (models.Student, error) {
-	return models.Student{}, nil
+	students := models.Student{}
+	database.DB.Db.Find(students, "id = ?", id)
+	return students, nil
 }
 
 func GetAllStudent() ([]models.Student, error) {
-	return nil, nil
+	students := []models.Student{}
+	return students, nil
 }
 
-func UpdateStudent(student models.Student) error {
+func UpdateStudent(student *models.Student) error {
+	database.DB.Db.Save(&student)
 	return nil
 }
 
-func DeleteStudent(student models.Student) error {
+func DeleteStudent(id string) error {
+	var student models.Student
+	res := database.DB.Db.Where("id = ?", id).Delete(&student)
+	if res.Error != nil {
+		log.Println("error deleting company: ", res.Error)
+		return res.Error
+	}
+	if res.RowsAffected == 0 {
+		log.Println("no company found with the given id")
+		return fmt.Errorf("no company found with id: %s", id)
+	}
 	return nil
 }
